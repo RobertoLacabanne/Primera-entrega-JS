2cfca830650555cb5f45377967546786c9b707a3
Roberto Lacabanne <rlacabanne14@gmail.com>
2022-07-28 05:21:28 -0300

    avanzando

---
+++ js/comida.js
@@ -0,0 +1,54 @@
+const items = document.getElementById("items");
+const templateCard = document.createElement("templateCard").content
+const fragment = document.createElementFragment
+let carrito = {}
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+})
+items.addEventListener("click", e =>  {
+    addCarrito(e)
+})
+
+const fetchData = async () => {
+    try {
+const res = await fetch ("api.json",)
+const data = await res.json()
+console.log (data)
+} catch (error) {
+     console.log(error)
+}
+}
+
+const pintarCards = data => {
+    data.forEach(producto => {
+        console.log(producto)
+        templateCard.querySelector("h5").texcontent = producto.title
+        templateCard.querySelector("p").texcontent = producto.precio
+        templateCard.querySelector("img").setAtribute("src",producto.thumbnailUrl)
+        templateCard.querySelector(".btn-dark").dataset.id = producto.id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    });
+items.appendChild(fragment)
+}
+const addCarrito = e => {
+    //console.log(e.target)
+    //console.log(e.target.classList.contains("btn-dark"))
+    if (e.target.classList.contains("btn-dark")) {
+    console.log(e.target.parentElement) 
+    setCarrito (e.target.parentElement)
+    }
+    e.stopPropagation();
+}
+const setCarrito = objeto => {
+const producto = {
+    id: objeto.querySelector(".btn-dark").dataset.id,
+    title: objeto.querySelector("h5").texcontent,
+    precio: objeto.querySelector("p").texcontent,
+    cantidad: 1
+}
+if (carrito.hasOwnProperty(producto.id)){
+    producto.cantidad = carrito [producto.id].cantidad + 1
+}
+carrito[producto.id] = {...producto}
+}
