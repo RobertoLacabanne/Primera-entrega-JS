6a089c748e95d37b38c48b78e89fd5a1a079591a
Roberto Lacabanne <rlacabanne14@gmail.com>
2022-07-30 07:32:45 -0300

    mejorando!

--- js/comida.js
+++ js/comida.js
@@ -1,58 +1,106 @@
-import data from "../api.json" assert { type: "json" };
-console.log(data);
+let allContainerCart = document.querySelector('.products');
+let containerBuyCart = document.querySelector('.card-items');
+let priceTotal = document.querySelector('.price-total')
+let amountProduct = document.querySelector('.count-product');
 
-const items = document.getElementById("items");
-const templateCard = document.createElement("templateCard").content
-const fragment = document.createElementFragment
-let carrito = {}
-document.addEventListener("DOMContentLoaded", () => {
-    fetchData();
-})
-items.addEventListener("click", e =>  {
-    addCarrito(e)
-})
 
-const fetchData = async () => {
-    try {
-const res = await fetch ("api.json",)
-const data = await res.json()
-console.log (data)
-} catch (error) {
-     console.log(error)
-}
+let buyThings = [];
+let totalCard = 0;
+let countProduct = 0;
+
+//functions
+loadEventListenrs();
+function loadEventListenrs(){
+    allContainerCart.addEventListener('click', addProduct);
+
+    containerBuyCart.addEventListener('click', deleteProduct);
 }
 
-const pintarCards = data => {
-    data.forEach(producto => {
-        console.log(producto)
-        templateCard.querySelector("h5").texcontent = producto.title
-        templateCard.querySelector("p").texcontent = producto.precio
-        templateCard.querySelector("img").setAtribute("src",producto.thumbnailUrl)
-        templateCard.querySelector(".btn-dark").dataset.id = producto.id
-        const clone = templateCard.cloneNode(true)
-        fragment.appendChild(clone)
-    });
-items.appendChild(fragment)
-}
-const addCarrito = e => {
-    //console.log(e.target)
-    //console.log(e.target.classList.contains("btn-dark"))
-    if (e.target.classList.contains("btn-dark")) {
-    console.log(e.target.parentElement) 
-    setCarrito (e.target.parentElement)
+function addProduct(e){
+    e.preventDefault();
+    if (e.target.classList.contains('btn-add-cart')) {
+        const selectProduct = e.target.parentElement; 
+        readTheContent(selectProduct);
     }
-    e.stopPropagation();
 }
-const setCarrito = objeto => {
-const producto = {
-    id: objeto.querySelector(".btn-dark").dataset.id,
-    title: objeto.querySelector("h5").texcontent,
-    precio: objeto.querySelector("p").texcontent,
-    cantidad: 1
+
+function deleteProduct(e) {
+    if (e.target.classList.contains('delete-product')) {
+        const deleteId = e.target.getAttribute('data-id');
+
+        buyThings.forEach(value => {
+            if (value.id == deleteId) {
+                let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
+                totalCard =  totalCard - priceReduce;
+                totalCard = totalCard.toFixed(2);
+            }
+        });
+        buyThings = buyThings.filter(product => product.id !== deleteId);
+        
+        countProduct--;
+    }
+    //FIX: El contador se quedaba con "1" aunque ubiera 0 productos
+    if (buyThings.length === 0) {
+        priceTotal.innerHTML = 0;
+        amountProduct.innerHTML = 0;
+    }
+    loadHtml();
 }
-if (carrito.hasOwnProperty(producto.id)){
-    producto.cantidad = carrito [producto.id].cantidad + 1
+
+function readTheContent(product){
+    const infoProduct = {
+        image: product.querySelector('div img').src,
+        title: product.querySelector('.title').textContent,
+        price: product.querySelector('div p span').textContent,
+        id: product.querySelector('a').getAttribute('data-id'),
+        amount: 1
+    }
+
+    totalCard = parseFloat(totalCard) + parseFloat(infoProduct.price);
+    totalCard = totalCard.toFixed(2);
+
+    const exist = buyThings.some(product => product.id === infoProduct.id);
+    if (exist) {
+        const pro = buyThings.map(product => {
+            if (product.id === infoProduct.id) {
+                product.amount++;
+                return product;
+            } else {
+                return product
+            }
+        });
+        buyThings = [...pro];
+    } else {
+        buyThings = [...buyThings, infoProduct]
+        countProduct++;
+    }
+    loadHtml();
+    //console.log(infoProduct);
 }
-carrito[producto.id] = {...producto}
+
+function loadHtml(){
+    clearHtml();
+    buyThings.forEach(product => {
+        const {image, title, price, amount, id} = product;
+        const row = document.createElement('div');
+        row.classList.add('item');
+        row.innerHTML = `
+            <img src="${image}" alt="">
+            <div class="item-content">
+                <h5>${title}</h5>
+                <h5 class="cart-price">${price}$</h5>
+                <h6>Amount: ${amount}</h6>
+            </div>
+            <span class="delete-product" data-id="${id}">X</span>
+        `;
+
+        containerBuyCart.appendChild(row);
+
+        priceTotal.innerHTML = totalCard;
+
+        amountProduct.innerHTML = countProduct;
+    });
 }
-pintarCards(comida)  
+ function clearHtml(){
+    containerBuyCart.innerHTML = '';
+ }  
